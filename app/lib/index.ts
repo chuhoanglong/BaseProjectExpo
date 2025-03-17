6e3b690263ee1af15b083e90eeea8d752e13eaa5
Dev_Pionero <dev_pionero@Mac-mini-cua-Dev-Pionero.local>
2025-03-17 11:21:20 +0700

    init project

--- app/lib/index.ts
+++
@@ -1,4 +0,0 @@
-import firebaseConfigAltDev from "./firebase/dev/firebaseAltId.json"
-import firebaseConfigAltProd from "./firebase/production/firebaseAltId.json"
-
-export const firebaseConfig = __DEV__ ? firebaseConfigAltDev : firebaseConfigAltProd
--- app/sdk/generated_sdk/models/GetUserConnectionEntityResponse.ts
+++
@@ -1,9 +0,0 @@
-/* generated using openapi-typescript-codegen -- do not edit */
-/* istanbul ignore file */
-/* tslint:disable */
-/* eslint-disable */
-import type { UserConnectionEntityServing } from './UserConnectionEntityServing';
-export type GetUserConnectionEntityResponse = {
-  userConnections: Array<UserConnectionEntityServing>;
-};
-
